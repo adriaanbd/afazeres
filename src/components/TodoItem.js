0f02c4a626cc71a2d66c0496dec03beb54670d59
mcrd25 <mayacrd25@gmail.com>
2019-09-02 18:59:22 -0400

    Created all getters and setters for main todo item attributes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,14 @@
 export default class TodoItem {
-	constructor(title, description, dueDate, priority) {
+	constructor(title, description, dueDate, priority = 'none') {
 		this.title = title;
 		this.description = description;
 		this.dueDate = dueDate;
 		this.priority = priority;
-	}
+		this.validPriority = (priority) => {
+			const priorities = ['none', 'low', 'medium', 'high'];
+			return priorities.includes(priority);
+		};
+	};
 
 	const getTitle = () => {
 		return this.title;
@@ -12,5 +16,31 @@ export default class TodoItem {
 
 	const setTitle = (title) => {
 		this.title = title;
+	};
+
+	const getDescription = () => {
+		return this.description;
+	};
+
+	const setDescription = (des) => {
+		this.description = des;
+	};
+
+	const getDueDate = () => {
+		return this.dueDate;
+	};
+
+	const setDueDate = (date) => {
+		this.dueDate = date;
+	};
+
+	const getPriority = () => {
+		return this.priority;
+	};
+
+	const setPriority = (priority) => {
+		if (this.validPriority(priority)) {
+			this.priority = priority;
+		}
 	};
 }
