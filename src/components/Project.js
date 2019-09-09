991aab6ad65b47babd12cd9dcb8420064fc44c1e
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-09 17:48:26 -0500

    WIP project class

---
+++ src/components/Project.js
@@ -0,0 +1,23 @@
+export default class Project {
+  constructor(title, description = '') {
+    this.title = title;
+    this.description = description;
+    this.toDoList = [];
+  }
+
+  getTitle() {
+    return this.title;
+  }
+
+  getDescription() {
+    return this.description;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  setDescription(description) {
+    this.description = description;
+  }
+}
