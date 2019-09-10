eee76df23e1ed1b537b8279993463ba00cc0ccab
Adriaan Beiertz <adriaanbd@gmail.com>
2019-09-10 18:27:28 -0500

    fixed formatting issues

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: `/\.scss$/`,
+        test: '/.scss$/',
         use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates css to CommonJS
-          "sass-loader" // compiles Sass to CSS
-        ]
-      }
-    ]
-  }
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates css to CommonJS
+          'sass-loader', // compiles Sass to CSS
+        ],
+      },
+    ],
+  },
 };
