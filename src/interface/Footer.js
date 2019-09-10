351ecc166fd9fd9efaa7d2a4e37e3c5caa51f016
mcrd25 <mayacrd25@gmail.com>
2019-09-10 19:44:38 -0400

    Created footer class

---
+++ src/interface/Footer.js
@@ -0,0 +1,15 @@
+export default class Footer {
+  constructor(title, year) {
+    this.footer = document.createElement('footer');
+    this.p = document.createElement('p');
+    this.footetText = () => `${title} &copy;${year}`;
+  }
+
+  generateFooter() {
+    this.footer.className = 'section darken-2 white-text center';
+    this.p.className = 'flow-text';
+    this.p.innerHTML = this.footetText();
+    this.footer.appendChild(this.p);
+    return this.footer;
+  }
+}
