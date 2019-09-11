f94253a44da5f79b5756f828b0f6beb9dfabbe2f
mcrd25 <mayacrd25@gmail.com>
2019-09-11 17:21:54 -0400

    WIP: MainContent class

---
+++ src/interface/MainContent.js
@@ -0,0 +1,14 @@
+export default class MainContent {
+	constructor(parent, content) {
+		this.content = content;
+		this.parent = parent;
+	}
+
+	setContent(content) {
+		this.content = content;
+	}
+
+	getContent() {
+		return this.content;
+	}
+}
